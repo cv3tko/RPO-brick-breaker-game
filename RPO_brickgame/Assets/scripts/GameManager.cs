96e77a6940c22617872985745e46890bcbc3d4b4
cv3tko <cvejtko@gmail.com>
2017-10-27 15:08:25 +0200

    dodan game over scene

---
+++ RPO_brickgame/Assets/scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameOver = false;
+    
+    public void endGame()
+    {
+        if(!gameOver)
+        {
+            gameOver = true;
+            Debug.Log("Game over");
+            SceneManager.LoadScene("gameOver");
+        }
+
+    }
+
+}
---
+++ RPO_brickgame/Assets/scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(rb.position.y < -10f)
+        {
+            FindObjectOfType<GameManager>().endGame();
+        }
+	}
+}
